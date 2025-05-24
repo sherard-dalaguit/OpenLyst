fb3037088b080a5d06e8d30529e0e4563dabb295
sherard-dalaguit <sherard.softwaredev@gmail.com>
2025-05-24 16:42:50 -0700

    remodeled home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-5xl font-medium">Kumusta Po Kayo</h1>
-      <h1 className="text-3xl">Ang lahat ng tao'y isinilang na malaya at pantay-pantay sa karangalan</h1>
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-5xl font-medium mb-8">Remote Radar</h1>
+      <h1 className="text-3xl">Please be patient with us as we remodel the website</h1>
     </div>
   );
 }
