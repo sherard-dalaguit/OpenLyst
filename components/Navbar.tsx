69e537a0fe26532a0e9ba1094b4c1238a826681f
sherard-dalaguit <sherard.softwaredev@gmail.com>
2025-05-25 18:49:47 -0700

    fix navbar css

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,27 +7,29 @@ import { Button } from "@/components/ui/button";
 
 const Navbar = () => {
   return (
-    <div className="fixed top-0 left-0 right-0 flex justify-between items-center px-8 z-50">
-      <Link href="/" className="text-2xl font-medium">
-        <Image
-          src={"/logo-light.png"}
-          alt="logo"
-          width={200}
-          height={100}
-        />
-      </Link>
+    <div className="fixed inset-x-0 top-0 z-50">
+      <div className="relative flex justify-between items-center px-4">
+        <Link href="/" className="flex-shrink-0 text-2xl font-medium">
+          <Image
+            src={"/logo-light.png"}
+            alt="logo"
+            width={200}
+            height={100}
+          />
+        </Link>
 
-      <h1 className="text-2xl">Search bar</h1>
+        <h1 className="absolute left-1/2 -translate-x-1/2 text-center text-xl">Search bar</h1>
 
-      <Link href="/">
-        <Button
-          variant="ghost"
-          size="icon"
-          className="[&_svg]:!w-6 [&_svg]:!h-6"
-        >
-          <IconUserCircle />
-        </Button>
-      </Link>
+        <Link href="/" className="flex-shrink-0">
+          <Button
+            variant="ghost"
+            size="icon"
+            className="[&_svg]:!w-8 [&_svg]:!h-8"
+          >
+            <IconUserCircle />
+          </Button>
+        </Link>
+      </div>
     </div>
   );
 };
