fec83e0192e70390163c820bfa9dcf98f36d0b43
sherard-dalaguit <sherard.softwaredev@gmail.com>
2025-06-09 14:44:32 -0700

    updated main landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,32 @@
+import LitButton from '@/components/ui/LitButton';
+import { IconBriefcase, IconArrowDown } from '@tabler/icons-react';
+import {Spotlight} from "@/components/ui/spotlight";
+
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-5xl font-medium mb-8">Remote Radar</h1>
-      <h1 className="text-3xl">Please be patient with us as we remodel the website</h1>
+    <div className="flex flex-col items-center justify-center text-center max-w-6xl mx-auto w-screen overflow-x-hidden h-screen">
+      <Spotlight />
+      <h1 className="text-8xl font-semibold">
+        Turn your <span className="primary-text-gradient">inbox</span> into the ultimate <span className="primary-text-gradient">remote board</span>
+      </h1>
+      
+      <p className="text-2xl max-w-4xl my-16">
+        Remote Radar pulls in hundreds of remote job listings from around the web and sends them to your inbox every day.
+      </p>
+
+      <div className="flex flex-col sm:flex-row gap-4 justify-center items-center">
+        <LitButton
+          title="Browse Jobs"
+          icon={<IconBriefcase />}
+          position="right"
+        />
+
+        <LitButton
+          title="Sign Up Free"
+          icon={<IconArrowDown />}
+          position="right"
+        />
+      </div>
     </div>
   );
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,14 +4,17 @@ import Link from "next/link";
 import Image from "next/image";
 import {IconUserCircle} from "@tabler/icons-react";
 import { Button } from "@/components/ui/button";
+import {ModeToggle} from "@/components/Theme";
+import { useTheme } from "next-themes";
 
 const Navbar = () => {
+  const { theme } = useTheme();
   return (
     <div className="fixed inset-x-0 top-0 z-50">
       <div className="relative flex justify-between items-center px-4">
         <Link href="/" className="flex-shrink-0 text-2xl font-medium">
           <Image
-            src={"/logo-light.png"}
+            src={theme == "dark" ? "/logo-dark.png" : "/logo-light.png"}
             alt="logo"
             width={200}
             height={100}
@@ -20,15 +23,18 @@ const Navbar = () => {
 
         <h1 className="absolute left-1/2 -translate-x-1/2 text-center text-xl">Search bar</h1>
 
-        <Link href="/" className="flex-shrink-0">
-          <Button
-            variant="ghost"
-            size="icon"
-            className="[&_svg]:!w-8 [&_svg]:!h-8"
-          >
-            <IconUserCircle />
-          </Button>
-        </Link>
+        <div className="flex items-center gap-4">
+          <ModeToggle />
+          <Link href="/" className="flex-shrink-0">
+            <Button
+              variant="ghost"
+              size="icon"
+              className="[&_svg]:!w-8 [&_svg]:!h-8"
+            >
+              <IconUserCircle />
+            </Button>
+          </Link>
+        </div>
       </div>
     </div>
   );
