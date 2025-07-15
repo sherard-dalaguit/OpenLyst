9db58dbfefbf557a59db105fd0f44ac808b9eca6
sherard-dalaguit <sherard.softwaredev@gmail.com>
2025-07-15 12:41:23 -0700

    minor eslint fix

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,10 +10,15 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
+  {
+    ignores: ["components/ui/**/*"],
+  },
   ...compat.config({
     extends: ["next/core-web-vitals", "next/typescript"],
     rules: {
-      "@typescript-eslint/no-explicit-any": "off"
+      "@typescript-eslint/no-explicit-any": "off",
+      '@typescript-eslint/ban-ts-comment': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
     },
   }),
 ];
