4db4be378ecb825535206806c5c59d9b85eb51e2
sherard-dalaguit <sherard.softwaredev@gmail.com>
2025-07-17 12:36:34 -0700

    Setup MongoDB & Mongoose

---
+++ lib/mongoose.ts
@@ -0,0 +1,52 @@
+import mongoose, { Mongoose } from 'mongoose';
+import logger from "@/lib/logger";
+import '@/database';
+
+const MONGODB_URI = process.env.MONGODB_URI as string;
+
+if (!MONGODB_URI) {
+	throw new Error('Please define the MONGODB_URI variable');
+}
+
+interface MongooseCache {
+	conn: Mongoose | null;
+	promise: Promise<Mongoose> | null;
+}
+
+declare global {
+	var mongoose: MongooseCache;
+}
+
+let cached = global.mongoose;
+
+if (!cached) {
+	cached = global.mongoose = { conn: null, promise: null };
+}
+
+const dbConnect = async (): Promise<Mongoose> => {
+	if (cached.conn) {
+		logger.info("Using existing mongoose connection");
+		return cached.conn;
+	}
+
+	if (!cached.promise) {
+		cached.promise = mongoose
+			.connect(MONGODB_URI, {
+				dbName: 'RemoteRadar',
+			})
+			.then((result) => {
+				logger.info("Connected to MongoDB");
+				return result;
+			})
+			.catch((error) => {
+				logger.error("MongoDB connection error:", error);
+				throw error;
+			})
+	}
+
+	cached.conn = await cached.promise;
+
+	return cached.conn;
+};
+
+export default dbConnect;
