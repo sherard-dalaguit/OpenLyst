71fb286fe1615e676703be17ddd64b8dc594f5a5
sherard-dalaguit <sherard.softwaredev@gmail.com>
2025-07-18 18:31:08 -0700

    Implemented Main Navbar

--- components/navigation/Navbar.tsx
+++ components/navigation/Navbar.tsx
@@ -8,34 +8,32 @@ import {ModeToggle} from "@/components/Theme";
 
 const Navbar = () => {
   return (
-    <div className="fixed inset-x-0 top-0 p-2 z-50">
-      <div className="relative flex justify-between items-center px-2">
-        <Link href="/" className="flex flex-row items-center text-2xl font-semibold">
-          <Image
-            src={"/logo.png"}
-            alt="logo"
-            width={48}
-            height={48}
-          />
-          <h1 className="primary-text-gradient">Remote Radar</h1>
+    <nav className="flex-between background-light900_dark200 fixed z-50 w-full gap-5 p-3 shadow-light-300 dark:shadow-none sm:px-12">
+      <Link href="/" className="flex flex-row items-center text-2xl font-semibold">
+        <Image
+          src={"/logo.png"}
+          alt="logo"
+          width={48}
+          height={48}
+        />
+        <h1 className="primary-text-gradient">Remote Radar</h1>
+      </Link>
+
+      <h1 className="absolute left-1/2 -translate-x-1/2 text-center text-xl">Search bar</h1>
+
+      <div className="flex-between gap-4">
+        <ModeToggle />
+        <Link href="/" className="flex-shrink-0">
+          <Button
+            variant="ghost"
+            size="icon"
+            className="[&_svg]:!w-8 [&_svg]:!h-8"
+          >
+            <IconUserCircle />
+          </Button>
         </Link>
-
-        <h1 className="absolute left-1/2 -translate-x-1/2 text-center text-xl">Search bar</h1>
-
-        <div className="flex items-center gap-4">
-          <ModeToggle />
-          <Link href="/" className="flex-shrink-0">
-            <Button
-              variant="ghost"
-              size="icon"
-              className="[&_svg]:!w-8 [&_svg]:!h-8"
-            >
-              <IconUserCircle />
-            </Button>
-          </Link>
-        </div>
       </div>
-    </div>
+    </nav>
   );
 };
 
