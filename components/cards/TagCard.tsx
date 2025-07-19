27be249e97e2e28f31acd8655aaef4e4e94e9bc0
sherard-dalaguit <sherard.softwaredev@gmail.com>
2025-07-18 20:15:50 -0700

    Implemented Right Sidebar

---
+++ components/cards/TagCard.tsx
@@ -0,0 +1,28 @@
+import Link from "next/link";
+import ROUTES from "@/constants/routes";
+import {Badge} from "@/components/ui/badge";
+
+interface Props {
+	_id: string;
+	name: string;
+	questions: number;
+	showCount?: boolean;
+	compact?: boolean;
+}
+
+const TagCard = ({ _id, name, questions, showCount, compact }: Props) => {
+
+	return (
+		<Link href={ROUTES.TAG(_id)} className="flex justify-between gap-2">
+			<Badge className="subtle-medium background-light800_dark300 text-light400_light500 rounded-md border-none px-4 py-2 uppercase">
+				<div className="flex-center space-x-2">
+					<span>{name}</span>
+				</div>
+			</Badge>
+
+			{showCount && <p className="small-medium text-dark500_light700">{questions}</p>}
+		</Link>
+	)
+}
+
+export default TagCard;
