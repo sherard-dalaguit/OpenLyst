f1b2db365c3b608f5b585b67e7e332c82f8f978e
sherard-dalaguit <sherard.softwaredev@gmail.com>
2025-08-09 19:53:45 -0700

    Changed API call to normal function call

--- app/api/run-all/route.ts
+++ app/api/run-all/route.ts
@@ -1,10 +1,11 @@
 import { api } from '@/lib/api';
 import { NextResponse } from 'next/server';
+import {runAllTests} from "@/scripts/testScraper";
 
 export async function GET() {
 	console.log('[api/run-all] -> handler hit');
 
-	await api.cron.runScraper();
+	await runAllTests();
 	await api.cron.dailyDigest();
 	await api.cron.weeklyDigest();
 
