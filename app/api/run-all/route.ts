db564e01bac29a011612cf0fb8aedcb0ddc839e2
sherard-dalaguit <sherard.softwaredev@gmail.com>
2025-08-09 20:13:08 -0700

    Changed singular function call into individual scraper calls

--- app/api/run-all/route.ts
+++ app/api/run-all/route.ts
@@ -1,11 +1,25 @@
 import { api } from '@/lib/api';
 import { NextResponse } from 'next/server';
-import {runAllTests} from "@/scripts/testScraper";
+import {
+	testJavascriptJobsScraper,
+	testJobspressoScraper,
+	testRemoteOkScraper,
+	testRemotiveScraper,
+	testSkipTheDriveScraper,
+	testWeWorkRemotelyScraper,
+	testWorkingNomadsScraper
+} from "@/scripts/testScraper";
 
 export async function GET() {
 	console.log('[api/run-all] -> handler hit');
 
-	await runAllTests();
+	await testWorkingNomadsScraper();
+  await testSkipTheDriveScraper();
+  await testWeWorkRemotelyScraper();
+  await testJobspressoScraper();
+  await testJavascriptJobsScraper();
+  await testRemoteOkScraper();
+  await testRemotiveScraper();
 	await api.cron.dailyDigest();
 	await api.cron.weeklyDigest();
 
