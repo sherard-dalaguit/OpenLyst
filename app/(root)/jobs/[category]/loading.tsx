028a6c40895fbef52d51ce1e4600114b32c56ffb
sherard-dalaguit <sherard.softwaredev@gmail.com>
2025-09-04 12:15:13 -0700

    Added Loading Screens ('reduces' wait for data fetch)

---
+++ app/(root)/jobs/[category]/loading.tsx
@@ -0,0 +1,41 @@
+'use client';
+
+import { Skeleton } from '@/components/ui/skeleton'
+import React from 'react'
+import {formatCategoryTitle} from "@/lib/utils";
+import { useParams } from 'next/navigation';
+
+const Loading = () => {
+	const { category } = useParams<{category: string}>();
+	return (
+		<section>
+			<div className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
+				<h1 className="h1-bold text-dark100_light900">{formatCategoryTitle(category)} Jobs</h1>
+			</div>
+
+			<div className="mb-10 mt-11 flex flex-wrap items-center justify-between gap-5">
+				<Skeleton className="h-14 flex-1"/>
+				<div className="hidden max-md:block">
+					<Skeleton className='h-14 w-28' />
+				</div>
+			</div>
+
+			<div className="my-10 hidden flex-row gap-3 md:flex">
+				<Skeleton className="h-9 w-28" />
+				<Skeleton className="h-9 w-28" />
+				<Skeleton className="h-9 w-47" />
+				<Skeleton className="h-9 w-33" />
+				<Skeleton className="h-9 w-25" />
+				<Skeleton className="h-9 w-32" />
+			</div>
+
+			<div className="flex w-full flex-col lg:flex-row lg:flex-wrap gap-6">
+				{[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((item) => (
+					<Skeleton key={item} className="h-48 lg:w-48/100 rounded-xl" />
+				))}
+			</div>
+		</section>
+	)
+}
+
+export default Loading;
---
+++ app/(root)/saved-jobs/loading.tsx
@@ -0,0 +1,35 @@
+import { Skeleton } from '@/components/ui/skeleton'
+import React from 'react'
+
+const Loading = () => {
+	return (
+		<section>
+			<div className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
+				<h1 className="h1-bold text-dark100_light900">Saved Jobs</h1>
+			</div>
+
+			<div className="mb-10 mt-11 flex flex-wrap items-center justify-between gap-5">
+				<Skeleton className="h-14 flex-1"/>
+				<div className="hidden max-md:block">
+					<Skeleton className='h-14 w-28' />
+				</div>
+			</div>
+
+			<div className="my-10 hidden flex-row gap-3 md:flex">
+				<Skeleton className="h-9 w-28" />
+				<Skeleton className="h-9 w-28" />
+				<Skeleton className="h-9 w-47" />
+				<Skeleton className="h-9 w-33" />
+				<Skeleton className="h-9 w-25" />
+			</div>
+
+			<div className="flex w-full flex-col lg:flex-row lg:flex-wrap gap-6">
+				{[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((item) => (
+					<Skeleton key={item} className="h-48 lg:w-48/100 rounded-xl" />
+				))}
+			</div>
+		</section>
+	)
+}
+
+export default Loading;
